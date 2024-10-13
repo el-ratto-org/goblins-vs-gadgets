ca7357f324812611e89001c76f62457fd8bfbb9c
Moe <107603838+Moe-Cherry@users.noreply.github.com>
2024-10-13 17:53:07 +1100

    test world turret update

--- turrets/basic_turret/basic_turret.gd
+++ turrets/basic_turret/basic_turret.gd
@@ -8,17 +8,23 @@ extends Node3D
 @export var damage = 1
 
 var can_shoot = true
+var turret_cell: Node3D
 
-func _ready() -> void:
-	# set turret position to cell
-	#global_position = GameManager.placement.cell
-	
-	var turret_cell = GameManager.placement.cell_position
-	if GameManager.placement.enemy_lanes[turret_cell.y] and can_shoot:
+
+#func _ready() -> void:
+	# set turret position to cell on board
+	#global_position = turret_cell.position
+	#if GameManager.placement.enemy_lanes[turret_cell.cell_position.y] and can_shoot:
+		#spawn_bullet()
+		#can_shoot = false
+		#$fire_rate_timer.start()
+
+func _process(delta: float) -> void:
+	# for test node
+	if can_shoot:
 		spawn_bullet()
 		can_shoot = false
 		$fire_rate_timer.start()
-
 
 func _on_fire_rate_timer_timeout() -> void:
 	can_shoot = true
