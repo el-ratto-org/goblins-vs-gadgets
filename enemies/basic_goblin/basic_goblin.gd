1321f8a0efcae3b7f8915396c6867ce35379985f
Moe <107603838+Moe-Cherry@users.noreply.github.com>
2024-10-20 21:18:30 +1100

    turret damage logic & hp bar added

--- enemies/basic_goblin/basic_goblin.gd
+++ enemies/basic_goblin/basic_goblin.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @export var damage: float = 1
 @export var speed: float = 1
 @export var health: int = 3
-
+@onready var attack_timer = $AttackArea/AttackTimer
 
 # Set when instantiated
 var lane
@@ -29,18 +29,33 @@ func _exit_tree() -> void:
 func _physics_process(delta: float) -> void:
 	velocity.x = -speed
 	move_and_slide()
+	
+	damage_turrt(delta)
 
 
-func _on_attack_area_entered(area: Area3D) -> void:
-	print("turret damaged")
-	if area.owner.is_in_group("Turret"):
-		speed = 0
-		# TODO: Speed needs to be set back to 1 once the turret is dead
-
-func _on_hurt_box_area_entered(area: Area3D) -> void:
+func _on_hit_box_area_entered(area: Area3D) -> void:
 	#apply damage to self, you got shot
 	if area.owner.is_in_group("bullet"):
 		if health:
 			health -= area.owner.damage
 		else:
 			queue_free()
+
+
+var turret_attacked: Node3D = null
+func _on_attack_area_entered(area: Area3D) -> void:
+	if !area.owner.is_in_group("bullet"):
+		speed = 0
+		turret_attacked = area.owner
+
+
+func damage_turrt(delta):
+	if turret_attacked:
+		turret_attacked.turret_hp -= damage * delta
+		print(turret_attacked.turret_hp)
+
+func _on_attack_area_exited(area: Area3D) -> void:
+	# mask set to only turret
+	# if this runs then a turret broke
+	speed = 1
+	turret_attacked = null
--- turrets/basic_turret/basic_turret.gd
+++ turrets/basic_turret/basic_turret.gd
@@ -1,17 +1,23 @@
 extends Node3D
 
-@export var turret_hp = 10
+@export var turret_hp = 3
 
 @onready var shoot_timer = $ShootTimer
 @onready var barrel_marker = $BarrelMarker
 @onready var model_animation = $BaseTurret1/AnimationPlayer
 @onready var fire_turret_sfx = $FireTurretSfx
-@onready var damage_timeout = $HitBox/CheckDamgeTimeout
+@onready var damage_timeout = $HitBox/CheckDamageTimeout
+@onready var health_bar = $HealthBar/SubViewport/TextureProgressBar
+
 
 var bullet_scene = preload("./basic_bullet.tscn")
 
 # Set when instantiated
 var index
+
+
+func _ready() -> void:
+	health_bar.max_value = turret_hp
 
 
 func spawn_bullet():
@@ -35,20 +41,9 @@ func _on_shoot_timer_timeout():
 		shoot_timer.start()
 
 
-var hurtbox_area: Area3D
-func _on_hurtbox_area_entered(area: Area3D) -> void:
-	#apply damage to self, you got smacked
-	if area.owner.is_in_group("Enemy"):
-		hurtbox_area = area
-		_on_check_damge_timeout()
-
-
-func _on_check_damge_timeout() -> void:
-	if turret_hp and is_instance_valid(hurtbox_area):
-		turret_hp -= hurtbox_area.owner.damage
-		print(turret_hp)
-		damage_timeout.start()
-	else:
-		queue_free()
-		# TODO: Needs to tell cell there isnt a turret there anymore
-		#GameManager.placement.board_cells.cell.remove_gadget(self)
+func _process(delta: float) -> void:
+	# update hp bar
+	health_bar.value = turret_hp
+	
+	if turret_hp <= 0:
+		GameManager.placement.board_cells[index.y][index.x].remove_gadget()
